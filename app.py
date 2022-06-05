5c3259c7dc609d15c81106b5e1db30ee5d6a1b61
gornym <gornymaciek@gmail.com>
2022-06-05 15:38:49 +0200

    dodaje strone

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/kontakt')
+def kontakt():
+    return render_template('kontakt.html')
+
+@app.route('/cars')
+def cars():
+    return render_template('cars.html')
+
+@app.route('/motorcycle')
+def motorcycle():
+    return render_template('motorcycle.html')
+
+@app.route('/yacht')
+def yacht():
+    return render_template('yacht.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
